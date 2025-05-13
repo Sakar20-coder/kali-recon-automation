608553f5cf4cd03a25f2c6135fe256b0ecf6ab96
Sakar Mishra <sakarmishra0@gmail.com>
2025-05-13 07:56:52 +0000

    Updated Nmap Scan

--- modules/network_scan.sh
+++ modules/network_scan.sh
@@ -2,5 +2,5 @@
 echo -e "${YELLOW}[*] Starting Nmap scan...${NC}"
 echo -n "Enter target IP/host: "
 read target
-nmap -sV -T4 -oA results/scans/nmap_scan $target
+nmap -sV -T4 -oA -n -Pn -o results/scans/nmap_scan $target
 echo -e "${GREEN}[+] Scan saved to results/scans/${NC}"
